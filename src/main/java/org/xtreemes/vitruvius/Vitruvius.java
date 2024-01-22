4be0ef41b287b0e30604913155f05d4a81c44e0a
xtreemes <52927886+Xtreemes@users.noreply.github.com>
2024-01-21 22:04:25 -0700

    Preparation for development (oh boy math final!!)

--- src/main/java/org/xtreemes/vitruvius/Vitruvius.java
+++ src/main/java/org/xtreemes/vitruvius/Vitruvius.java
@@ -1,13 +1,19 @@
 package org.xtreemes.vitruvius;
 
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vitruvius extends JavaPlugin {
 
+    public static ComponentLogger LOGGER;
+    public static Plugin PLUGIN;
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.getComponentLogger().debug("Vitruvius enabled!");
+        PLUGIN = this;
+        LOGGER = this.getComponentLogger();
     }
 
     @Override
